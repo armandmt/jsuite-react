e9aea1517a3aaef9a367539dab4bd62db0780bc5
Armand Mata <amata3@xtec.cat>
2022-03-02 19:56:05 +0100

    1

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Tickets from './Tickets/Tickets'
+import Footer from './Common/Footer'
+import Header from './Common/Header'
+
+const App = () => {
+  return (
+    <>
+    
+    <div><Header/></div>
+    <div><Tickets/></div>
+    <div><Footer/></div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Tickets/Tickets.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../Common/styles.css"
+ 
+
+const Tickets = () => {
+  return (
+    <div className='centre'>Tickets</div>
+  )
+}
+
+export default Tickets
